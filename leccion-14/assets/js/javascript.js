a359b16ea833b86c47a16feddcd42142e69e4b06
nelyflores <nely.afloresr@gmail.com>
2017-03-22 21:47:11 -0600

    agregando boton de eliminar frutas

--- leccion-14/assets/js/javascript.js
+++ leccion-14/assets/js/javascript.js
@@ -1,17 +1,20 @@
 //funcion agregar lista
-function agregarlista()
-{
-  //declaro una variable que tomara el valor de la caja de texto
-  var nuevoLi=document.getElementById("nueva_lista").value;
-  if(nuevoLi.length>0)
+  function agregarlista()
   {
-    //crea un elemento HTML especificado por su tagname en este caso "li" sera un elemento de la lista
+  
+    var nuevoLi=document.getElementById("nueva_lista").value;
     var li=document.createElement("li");
-    //se le asigna un id
-    li.id=nuevoLi;
-    // se agrega el elemento a la lista ordenada 
     li.innerHTML= nuevoLi;
-   document.getElementById("listaOrdenada").appendChild(li);
+    document.getElementById("listaOrdenada").appendChild(li);
+    document.getElementById('nueva_lista').value="";
   }
 
-}
+
+  function eliminarLista()
+  {
+    var list = document.getElementById("listaOrdenada");
+    var elementos = document.getElementsByTagName("li").length;
+    console.log(elementos);
+    list.removeChild(list.childNodes[elementos]);
+
+  }
